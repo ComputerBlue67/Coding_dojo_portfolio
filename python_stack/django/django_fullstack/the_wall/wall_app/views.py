61fadfd8d657b11d4c17a91cf86c820b94331f34
Computerblue <ohsheet2373@gmail.com>
2021-05-29 18:35:43 -0700

    the wall first half again

--- python_stack/django/django_fullstack/the_wall/wall_app/views.py
+++ python_stack/django/django_fullstack/the_wall/wall_app/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def success(request):
+    if 'user' not in request.session:
+        return redirect('/')
+    context = {
+        'wall_messages': Wall_message.objects.all()
+    }
+    return render(request, 'success.html', context)
+## Logging in and registering
+
+def register(request):
+    print(request.POST)
+    # Create a user object
+
+    errors = User.objects.basic_validator(request.POST)
+    print(errors)
+    if len(errors)>0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    new_user = User.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], email=request.POST['email'], password=request.POST['pw'])
+    request.session['user'] = new_user.first_name
+    request.session['id'] = new_user.id
+    return redirect('/success')
+
+def login(request):
+    print(request.POST)
+    # retrieving a user from the db
+    logged_user = User.objects.filter(email=request.POST['email'])
+    if len(logged_user) > 0:
+        logged_user = logged_user[0]
+        if logged_user.password == request.POST['pw']:
+            request.session['user'] = logged_user.first_name
+            request.session['id'] = logged_user.id
+            return redirect('/success')
+    return redirect('/')
+
+def logout(request):
+    print(request.session)
+    request.session.flush()
+    print(request.session)
+    return redirect('/')
+
+def post_mess(request):
+    Wall_message.objects.create(message=request.POST['mess'], poster=User.objects.get(id=request.session['id']))
+    return redirect('/success')
